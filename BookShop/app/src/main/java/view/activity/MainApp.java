045cab28870d1032d5fef7679f469ac4555f5ae7
Hai000 <21130343@st.hcmuaf.edu.vn>
2024-05-14 22:13:53 +0700

    update view main and login

---
+++ BookShop/app/src/main/java/view/activity/MainApp.java
@@ -0,0 +1,33 @@
+package view.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.bookshop.R;
+
+public class MainApp extends AppCompatActivity {
+    Button btnLoginByGoogle,btnRegister,btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_app);
+        btnLogin = findViewById(R.id.login);
+        setOnClickLogin();
+    }
+    public void setOnClickLogin(){
+        if(btnLogin != null){
+            btnLogin.setOnClickListener(v -> {
+                startActivity(new Intent(this, LoginActivity.class));
+
+            });
+        }
+    }
+}
