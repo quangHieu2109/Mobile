f5c7d7c2aea74b1ff0692efeaba2146e585ab0dd
Hai000 <21130343@st.hcmuaf.edu.vn>
2024-05-18 18:26:14 +0700

    update sigin gooogle

--- BookShop/app/src/main/java/view/fragment/FragmentSigin.java
+++ BookShop/app/src/main/java/view/fragment/FragmentSigin.java
@@ -1,17 +1,33 @@
 package view.fragment;
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.bookshop.R;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import view.activity.HomeActivity;
 
@@ -21,11 +37,15 @@ import view.activity.HomeActivity;
  * create an instance of this fragment.
  */
 public class FragmentSigin extends Fragment {
-    Button btnForgetPass,btnSigin;
+    SignInClient signInClient;
+    BeginSignInRequest signInRequest;
+
+    Button btnForgetPass, btnSigin;
     ImageButton btnLoginByGoogle;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    ActivityResultLauncher<IntentSenderRequest> intentSender;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -68,24 +88,78 @@ public class FragmentSigin extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sigin, container, false);
+
         btnForgetPass = view.findViewById(R.id.btn_forgot_password);
         btnSigin = view.findViewById(R.id.btn_sign_in);
         btnLoginByGoogle = view.findViewById(R.id.btn_login_gg);
         setBtnClickListeners();
+        signInClient = Identity.getSignInClient(getActivity());
+        signInRequest = BeginSignInRequest.builder()
+                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                        .setSupported(true)
+                        .build())
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        .setServerClientId(getString(R.string.web_client_id))
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                .setAutoSelectEnabled(true)
+                .build();
+        intentSender = registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == getActivity().RESULT_OK) {
+                    try {
+                        SignInCredential credential = signInClient.getSignInCredentialFromIntent(result.getData());
+                        String idToken = credential.getGoogleIdToken();
+                        if (idToken != null) {
+                            String email = credential.getId();
+                            Toast.makeText(getActivity(), email, Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
         return view;
     }
+
     private void setBtnClickListeners() {
 
         btnForgetPass.setOnClickListener(v -> {
-          getActivity().getSupportFragmentManager().beginTransaction()
-                  .replace(R.id.container, new FragmentForgetPass())
-                  .setTransition(androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                  .addToBackStack("fragmentForgetPass")
-                  .commit();
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, new FragmentForgetPass())
+                    .setTransition(androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .addToBackStack("fragmentForgetPass")
+                    .commit();
         });
         btnSigin.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), HomeActivity.class);
             startActivity(intent);
         });
+        btnLoginByGoogle.setOnClickListener(v -> {
+            signInClient.beginSignIn(signInRequest).addOnSuccessListener(getActivity(), new OnSuccessListener<BeginSignInResult>() {
+                        @Override
+                        public void onSuccess(BeginSignInResult beginSignInResult) {
+                            try {
+                                IntentSenderRequest intentSenderRequest = new IntentSenderRequest.Builder(beginSignInResult.getPendingIntent().getIntentSender()).build();
+                                intentSender.launch(intentSenderRequest);
+                            } catch (Exception e) {
+                                Log.d("TAG", e.getLocalizedMessage());
+                            }
+
+                        }
+                    })
+                    .addOnFailureListener(getActivity(), new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("TAG", "Error signing in with Google: " + e.getMessage());
+                        }
+                    });
+        });
     }
+
+
 }
