7e9d332759ac4ef21b3395c0d17b7559eb505276
Hai000 <21130343@st.hcmuaf.edu.vn>
2024-06-15 22:03:15 +0700

    update vnpay

---
+++ BookShop/app/src/main/java/api/vnpay/TypeAction.java
@@ -0,0 +1,11 @@
+package api.vnpay;
+
+public enum TypeAction {
+    SUCCESS,
+    UNFINISHED,
+    PENDING,
+    FAILED,
+    CHEAT,
+    REFUND_REFUSED,
+    UNKNOWN
+}
--- BookShop/app/src/main/java/view/fragment/FragmentBookDetail.java
+++ BookShop/app/src/main/java/view/fragment/FragmentBookDetail.java
@@ -1,8 +1,8 @@
 package view.fragment;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,7 +26,7 @@ import api.AApi;
 import api.APIService;
 import api.BookResponse;
 import api.Login;
-import api.VNPaySDK;
+import api.vnpay.VNPaySDK;
 import loader.BookLoader;
 import loader.BookType;
 import model.Book;
@@ -175,7 +175,7 @@ public class FragmentBookDetail extends Fragment {
             }
         });
         btn_buy.setOnClickListener(v -> {
-            VNPaySDK.openSDK(getContext());
+            VNPaySDK.openSDK(getContext(), book.getProduct().getPrice());
         });
 
 
